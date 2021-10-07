a0e5ddfdd5bb6c4317296cc3a9110a30ce5ef7ed
halag66 <hala.gr11@gmail.com>
2021-10-08 00:16:26 +0300

    a working example

--- app.py
+++ app.py
@@ -1,4 +1,14 @@
 from flask import Flask, render_template
+import requests
+import xml.etree.ElementTree as ET
+
+def read_url():
+    link = 'http://www.ynet.co.il/Integration/StoryRss2.xml'
+    response = requests.get(link)
+    xml_data = response.text.encode('utf-8', 'ignore') 
+    tree = ET.parse(xml_data)
+    root = tree.getroot()
+    return xml_data
 
 app = Flask(__name__)
 @app.route("/")
@@ -6,4 +16,5 @@ def main():
     return render_template('index.html')
 
 if __name__ == "__main__":
+    #print(read_url())
     app.run()
